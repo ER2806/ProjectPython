7f59683216ceb3c1884af3c62cae582c8e09c9e7
Unknown <astahov.dl@mail.ru>
2017-04-08 04:48:49 +0300

    test deleted

--- Telegram/test.py
+++
@@ -1,9 +0,0 @@
-import telepot
-import GETKEY
-
-rf = open("key", "r");
-token = GETKEY.get()
-#print(token == '334638429:AAGFfw_ZJo8tN5nJBjAov8woxRkiFnqCnA0')
-TelegramBot = telepot.Bot(token);
-print(TelegramBot.getMe())
-
